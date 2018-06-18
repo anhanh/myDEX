c520e8502426f585c866c2fec8b8032b9745debe
Tuan Anh <tuananh.itc@gmail.com>
2018-06-18 23:43:20 +0700

    Unit test Withdraw Token

--- src/test/exchange_test.js
+++ src/test/exchange_test.js
@@ -13,7 +13,7 @@ contract('MyExchange', function(accounts) {
             exchange = instance;
             return exchange.addToken("TuanAnh", token.address);
         }).then(function () {
-            return exchange.hasToken.call("TuanAnh");
+            return exchange.hasToken("TuanAnh");
         }).then(function (hasToken) {
             assert.equal(hasToken, true, "The Token was not added");
         });
@@ -34,6 +34,22 @@ contract('MyExchange', function(accounts) {
             return exchange.getBalance("TuanAnh");
         }).then(function(balanceToken) {
             assert.equal(balanceToken, 2000, "There should be 2000 tokens for the address");
+        });
+    });
+
+    it("should be possible to Withdraw Token", function () {
+        var exchange;
+        var token;
+        return tuananh.deployed().then(function (instance) {
+            token = instance;
+            return myExchange.deployed();
+        }).then(function (instance) {
+            exchange = instance;
+            return exchange.withdrawToken("TuanAnh", 1000);
+        }).then(function() {
+            return exchange.getBalance("TuanAnh");
+        }).then(function(balanceToken) {
+            assert.equal(balanceToken, 1000, "There should be 0 tokens for the address");
         });
     });
 
